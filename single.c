15d761902fe8b538b98a9a02d808eb5e7b5c1d45
alexisveryreal <alexmaple20@gmail.com>
2020-01-05 12:39:21 -0800

    starting add_at function

--- single.c
+++ single.c
@@ -1,4 +1,56 @@
 /* single.c
  * Author: Alexis Villarreal
  * CreatedL 1/5/2020
+ *
+ * Singly linked list, using add_at, add_beginning, add_end, remove, dump?
  */
+
+
+	#include <stdio.h>
+	#include <stdlib.h>
+
+	/* struct for node */
+	typedef struct note_t {
+		int data;
+		struct node_t *next
+	} node_t;
+
+	node_t *head = NULL;
+
+
+	/******************************************/
+	/* Creates a new node, and puts it at a   */
+	/* certain position, checking all points  */
+	/******************************************/
+	void add_at (int pos, int data){
+
+		note_t *node = malloc(sizeof(note_t) * 1);
+		node -> data = data;
+		node -> next = NULL;
+
+		/* if empty */
+		if(head == NULL){
+			head = node;
+			return;
+		}
+
+		int index = 0;
+
+		node_t *prev = NULL;
+		node_t *cur = head;
+
+
+		/* loop until position found, OR end */
+		while (cur != NULL && index != pos){
+			++idx;
+			prev = cur;
+			cur = cur -> next;
+		}
+
+
+
+
+
+
+
+	}
