eb836ccfc4b78440248348051bf6c6096eea3884
alexisveryreal <alexmaple20@gmail.com>
2020-01-05 13:09:53 -0800

    Added add_at beginning,end, and print

--- single.c
+++ single.c
@@ -2,7 +2,7 @@
  * Author: Alexis Villarreal
  * CreatedL 1/5/2020
  *
- * Singly linked list, using add_at, add_beginning, add_end, remove, dump?
+ * Singly linked list, using add_at, add_beginning, add_end, remove, print_list
  */
 
 
@@ -47,19 +47,57 @@
 			cur = cur -> next;
 		}
 
-		/* head update */
+		/* head update, begin of list */
 		if (index == 0) {
 			head = node;
 			node -> next = cur;
 			return;
 		}
 
-		/* reached end */
+		/* end of list */
 		if (cur == NULL) {
 			prev -> next = node;
 			return;
 		}
 
+		/* middle of list */
 		prev -> next = node;
 		node -> next = cur;
 	}
+
+	/*********************************/
+	/* adds at the beginning of the  */
+	/* linked list, using the add_at */
+	/* function.                     */
+	/*********************************/
+	void add_beginning(int data){
+		add_at(0, data);
+	}
+
+	/***************************************/
+	/* adds at the end of the linked list, */
+	/* using the add_at function. Using -1 */
+	/* because it will never reach a pos,  */
+	/* meaning it will always go to NULL   */
+	/***************************************/
+	void add_end(int data){
+		add_at(-1, data);
+	}
+
+	/* Prints the list using temporary node */
+	void print_list(){
+		node_t *temp = head;
+
+		while(temp != NULL) {
+			printf("data: %d\n", temp->data);
+			temp = temp->next;
+		}
+
+	}
+
+
+
+
+
+
+
